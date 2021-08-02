bf52e6dc2629f6dce652c0bd6ff9a60fecdbefd0
Tom Draths <64986521+tdraths@users.noreply.github.com>
2021-08-02 19:57:22 +0100

    Add files via upload

---
+++ penguins_model_building.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+path = 'https://raw.githubusercontent.com/dataprofessor/streamlit_freecodecamp/main/app_8_classification_penguins/penguins_cleaned.csv'
+
+penguins = pd.read_csv(path)
+
+df = penguins.copy()
+target = 'species'
+encode = ['sex', 'island']
+
+for col in encode:
+    dummy = pd.get_dummies(df[col], prefix=col)
+    df = pd.concat([df, dummy], axis=1)
+    del df[col]
+
+target_mapper = {'Adelie': 0, 'Chinstrap': 1, 'Gentoo': 2}
+def target_encode(val):
+    return target_mapper[val]
+
+df['species'] = df['species'].apply(target_encode)
+
+X = df.drop('species', axis=1)
+Y = df['species']
+
+from sklearn.ensemble import RandomForestClassifier
+clf = RandomForestClassifier()
+clf.fit(X, Y)
+
+import pickle
+pickle.dump(clf, open('penguins_clf.pkl', 'wb'))
